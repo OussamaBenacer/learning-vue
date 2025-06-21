16b91c20f65f66747abbb52b17a2ef73dd082468
BENACER-Oussama-Imad-Eddine <o_benacer@estin.dz>
2025-06-21 17:40:47 +0100

    add the refresh logic into api interceptors response if error status 401

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
-import { useAuthStore } from "@/stores/authStore";
 import axios from "axios";
+import { useAuthStore } from "@/stores/authStore";
+import { refreshApi } from "@/services/auth";
+import router from "@/router";
 
 const api = axios.create({
   baseURL: "https://api.escuelajs.co/api/v1",
   timeout: 10000,
 });
 
-// add token if exist
+// add token in the request if exist
 api.interceptors.request.use(
   (config) => {
     const auth = useAuthStore();
@@ -16,6 +18,32 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// try to refresh the token and resend the request . if the token expires
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await refreshApi();
+        return api(originalRequest);
+      } catch (err) {
+        console.error("Refresh token failed", err);
+        // expired session
+        alert("Your session has expired, please login again.");
+        const auth = useAuthStore();
+        auth.logout();
+        router.push("/");
+      }
+    }
+
     return Promise.reject(error);
   },
 );
--- src/services/auth/logoutApi.js
+++ src/services/auth/logoutApi.js
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/stores/authStore";
 
 export const logoutApi = async () => {
   const auth = useAuthStore();
-  auth.setTokens("", "");
+  auth.logout();
 
   // there is no endpoint for logout (remove refresh token from db)
 };
